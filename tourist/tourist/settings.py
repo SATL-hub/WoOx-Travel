5d4d86472ddb8e9576c03edd2307362d8e6dd119
SATL-hub <santeena.at@gmail.com>
2025-05-08 11:29:49 +0530

    Update settings.py

--- tourist/tourist/settings.py
+++ tourist/tourist/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u@v_x(yhsbt)2cmpubk8-tmc8lmhlm$jbwpy&4(9vf=&j*2e%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.9', '127.0.0.1', '0.0.0.0','localhost','*']
 
